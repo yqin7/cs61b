fa54c607adf3a9438a012d56f417707950662d72
Ye Qin <wolfmgjky1@gmail.com>
2023-09-28 23:05:02 -0400

    default is empty size done

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -3,7 +3,12 @@ package deque;
 public interface Deque<T> {
     public void addFirst(T item);
     public void addLast(T item);
-    public boolean isEmpty();
+    default public boolean isEmpty() {
+        if (this.size() == 0) {
+            return true;
+        }
+        return false;
+    }
     public int size();
     public void printDeque();
     public T removeFirst();
