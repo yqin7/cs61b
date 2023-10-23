36b44d92760858176e260b0f0687ec0efe65ad61
Ye Qin <wolfmgjky1@gmail.com>
2023-10-22 23:28:45 -0400

    lab7 test all methods

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -28,22 +28,60 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return containsKey(root, key);
+    }
+
+    public boolean containsKey(Node node, K key) {
+        if (node == null) {
+            return false;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp > 0) {
+            return containsKey(node.right, key);
+        } else if (cmp < 0) {
+            return containsKey(node.left, key);
+        }
+        return true;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return get(root, key);
+    }
+
+    private V get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp > 0) {
+            return get(node.right, key);
+        } else if (cmp < 0) {
+            return get(node.left, key);
+        }
+        return node.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void put(K key, V value) {
+        put(root, key, value);
+    }
 
+    public void put(Node node, K key, V value) {
+        if (node == null) {
+            node = new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp > 0) {
+            put(node.right, key, value);
+        } else if (cmp < 0) {
+            put(node.left, key, value);
+        }
     }
 
     @Override
@@ -51,7 +89,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         return null;
     }
 
+    public void printInOrder() {
 
+    }
 
 
 
